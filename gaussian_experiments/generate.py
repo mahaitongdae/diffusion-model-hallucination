81c6b21c7a3c61e84ebf527d7572bba06058b0ef
haitong_office <mhtmht47@gmail.com>
2025-02-24 21:03:11 -0500

    debug ode solver

--- gaussian_experiments/generate.py
+++ gaussian_experiments/generate.py
@@ -1,29 +1,35 @@
 import numpy as np
 from ddpm_torch.toy import *
 import matplotlib.pyplot as plt
+import torch
 
 if __name__ == '__main__':
+    model_path = '/home/haitong/PycharmProjects/diffusion-model-hallucination/gaussian_experiments/chkpts/edm_UnbalancedGaussian2D_200000_rssm_uniform_0/ddpm_UnbalancedGaussian2D_gen_0.pt'
+    # replace with model_path
+
     trainloader = DataStreamer("UnbalancedGaussian2D", batch_size=10000, num_batches=2, modes=2)
     evaluator = Evaluator(
         true_data=np.concatenate([
             next(iter(trainloader)) for _ in range(2)
         ]), eval_batch_size=1000, max_eval_count=2000, value_range=[0, 10])
 
-    betas = get_beta_schedule(
-        "linear", beta_start=0.001, beta_end=0.01, timesteps=1000)
-
-    diffusion = GaussianDiffusion(
-        betas=betas, model_mean_type="eps", model_var_type="fixed-large", loss_type="mse")
+    # diffusion = GaussianDiffusion(
+    #     betas=betas, model_mean_type="eps", model_var_type="fixed-large", loss_type="mse")
+    diffusion = EDMDiffusion(sigma_min=0.001, sigma_max=30, sample_steps=20, device=torch.device("cuda"))
+    # These are sigma_min, sigma_max, sample_step in train_toy_edm.py, feel free to change if you change the training hyperparameters.
+    model = Decoder(2, 128, 3).to(torch.device("cuda"))
+    # mid_features, num_temporal layers in hyperparameters.
+    model.load_state_dict(torch.load(model_path)['model'])
 
 
     def denoise_fn(x_t, t):
-        return diffusion.get_true_score_unbalanced_gmm(x_t, t)
+        return model(x_t, t)
 
 
     def sample_fn(n):
         shape = (n,) + (2,)
-        sample = diffusion.p_sample_grad(
-            denoise_fn=denoise_fn, shape=shape, device="cpu", noise=None)
+        sample = diffusion.p_sample(
+            denoise_fn=denoise_fn, shape=shape, device="cuda", noise=None)
         return sample.detach().cpu().numpy()
 
         # if evaluator is not None:
@@ -64,10 +70,10 @@ if __name__ == '__main__':
     y_hist_ax.tick_params(axis='y', which='both', labelleft=False)
     y_hist_ax.set_xticks([0.2, 0.8])
 
-    plt.suptitle('Langevin dynamics sample \n with true score function')
+    # plt.suptitle('Langevin dynamics sample \n with true score function')
 
     # Adjust the layout to avoid overlaps
     plt.tight_layout()
 
     # Show the plot
-    plt.savefig('generate_langevin.pdf')
+    plt.show()
